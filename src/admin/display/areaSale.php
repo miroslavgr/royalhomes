d2c81e80cce99b85768e18d3bc047af3e2fff52b
miroslavgr <43068250+miroslavgr@users.noreply.github.com>
2018-09-12 16:53:16 +0300

    Add files via upload

---
+++ src/admin/display/areaSale.php
@@ -0,0 +1,23 @@
+action areaSale.php
+
+sql get all areas where sale
+
+filteredList=sqlresult
+
+
+if(POST[priceFrom] && priceTo
+{
+	for i filteredList i++
+		if(!POST[name] operation filteredList[i])
+			filteredList.remove(filteredList[i]
+		
+}
+elseif(...)
+{
+	iterate filteredList
+		if(!)
+			filteredList.remove(filteredList[i])
+			
+}
+
+SESSION[saleFilter]=filteredList;
